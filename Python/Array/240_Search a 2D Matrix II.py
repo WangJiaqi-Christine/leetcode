4a8935ccf6c94d180b44fdfae0739dc7ae27e902
jq-77 <wangjiaqi.christine@gmail.com>
2021-07-20 22:58:10 +0800

    modified:   Python/Array/240_Search a 2D Matrix II.py

--- Python/Array/240_Search a 2D Matrix II.py
+++ Python/Array/240_Search a 2D Matrix II.py
@@ -14,6 +14,29 @@ Integers in each row are sorted in ascending from left to right.
 Integers in each column are sorted in ascending from top to bottom.
 """
 
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        
+        i, j = len(matrix)-1, 0
+        
+        while (i >= 0) and (j <= len(matrix[0])-1):
+            if matrix[i][j] == target:
+                return True
+            
+            elif matrix[i][j] >= target:
+                i -= 1
+                
+            else:
+                j += 1
+        
+        return False
+    
+    
 ********偷懒一行废内存解法********
 class Solution(object):
     def searchMatrix(self, matrix, target):
@@ -22,4 +45,5 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
+        
         return any(target in row for row in matrix)
