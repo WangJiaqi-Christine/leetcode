b89245dd0965e3cf3344ce37c4b099043615bcf8
jq-77 <wangjiaqi.christine@gmail.com>
2021-07-24 23:56:27 +0800

    temp

---
+++ Python/Stack/739_Daily Temperatures.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 24 23:54:25 2021
+
+@author: keikei
+"""
+
+"""
+Given an array of integers temperatures represents the daily temperatures, 
+return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. 
+If there is no future day for which this is possible, keep answer[i] == 0 instead.
+"""
+
